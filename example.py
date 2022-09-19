954b0f388272c36821a7a68def15e616104d8de6
Kenny mac <kennychavez.mtl@gmail.com>
2022-09-19 13:25:49 -0400

    added comments to ensure the well functionning by users of the example.py file

--- example.py
+++ example.py
@@ -1,38 +1,29 @@
 # package imports
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
+# local imports
 from support_resistance_indicator import SupportResistanceIndicator
 
 # this is an example of how to run the python software for a given dataframe
 
-# 1) dataframe:
+# 1) Dataframe:
 # first you need a dataframe, we have one set as example in this repository but any dataframe
 # works as long as you have between 500-1500 points in the dataframe, this dataframe is loaded up using pickle
 df = pd.read_pickle('dataframe_example.pickle')
+
 
 # 2) Program Execution:
 # next we are going to pass the dataframe to the main app.py main method
 
 # we create a class instance to be able to use the methods of the class
-
-# variables
-print(f' the lenght of the dataframe is {len(df)}')
-
-# we create a class instance of the support resiststance indicator class
 support_resistance = SupportResistanceIndicator(df = df)
 
 # we call the main function which will return the support and resistance horizontal levels
 support_resistance_levels = support_resistance.main()
 
-# 
 
-print(f' the support levels are {support_resistance_levels}')
-print(df)
-
-
-# ~~~~~~~~~~~~~ Plot Part ~~~~~~~~~~~~ 
+# 3) Ploting:
 # you can choose to comment this part out if you dont need to visualize the data, otherwise
 # this allows you to see the support and resistance levels on a chart
 
